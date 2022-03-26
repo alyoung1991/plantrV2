486bd2780f852747b3f758bc1cd98d3ab5943fca
alberto <alyoung1991@gmail.com>
2022-03-25 21:15:02 -0400

    Added user auth

---
+++ app/models/user.rb
@@ -0,0 +1,42 @@
+class User < ApplicationRecord
+    validates :email, :password_digest, :name, :session_token, presence: true
+    validates :email, uniqueness: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+
+    after_initialize :ensure_session_token
+
+    attr_reader :password
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+
+        if user && user.is_password?(password)
+            return user
+        else
+            return nil
+        end
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
+    end
+end
