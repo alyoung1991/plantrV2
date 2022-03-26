6b66b5e3de21ea401fa6df5c13751a24835139dd
alberto <alyoung1991@gmail.com>
2022-03-26 04:05:29 -0400

    Bootstrapped current user to window

--- frontend/plantr.jsx
+++ frontend/plantr.jsx
@@ -2,14 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { login } from './actions/session_actions';
+import { login, logout } from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
-    const store = configureStore();
+    let store;
+
+    if(window.currentUser){
+        const preloadedState = {
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            },
+            session: { id: window.currentUser.id }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    }else{
+        store = configureStore();
+    }
 
     window.getState = store.getState;
     window.dispatch = store.dispatch;
     window.login = login;
+    window.logout = logout;
 
     const root = document.getElementById("root");
     ReactDOM.render(<Root store={store} />, root);
